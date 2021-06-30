1447569d3ddfe61a816d1ce3080c4eaddd51ce5c
kritikavarshney8859 <68337183+kritikavarshney8859@users.noreply.github.com>
2021-06-30 15:12:54 +0530

    This file is created by me.
    This will give the 3x3 matrix and also the sum of diagonal elements.

---
+++ Pointers.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+	int a[3][3],i,j,sum=0;
+	int *p[3][3];
+	
+	
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("Input elements in the matrix: element- [%d][%d]: ",i,j);
+		    scanf("%d",&a[i][j]);
+		    p[i][j]=&a[i][j];
+		}
+		
+	}
+	printf("The matrix is: \n");
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%5d",*p[i][j]);
+			
+			}	
+		printf("\n");
+
+	}
+	for(i=0;i<3;i++)
+	{
+		sum=sum+*p[i][i];
+	}
+	printf("Sum of 3x3 all diagonal element is: %d",sum);
+	
+}
